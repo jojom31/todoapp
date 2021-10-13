7bab9cd20209606404450ae6500a2a371a45b819
JoJo Myles <jo.myles3112@gmail.com>
2021-10-13 15:42:21 -0400

    created core, test and webserver folders and files

---
+++ TodoApp.Core/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TodoApp.Core
+{
+    public class TodoItem
+    {
+        public int TodoId { get; set;  }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+
+
+        public TodoItem (string title)
+        {
+            this.Title = title;
+            this.IsComplete = false;
+        }
+
+        public TodoItem(string title, bool isComplete)
+        {
+            this.Title = title;
+            this.IsComplete = isComplete;
+        }
+
+        void MarkComplete()
+        {
+            this.IsComplete = true;
+        }
+    }
+}
